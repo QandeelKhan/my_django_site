a4cb3c3683db20db74935b212a2e87d385ae8c73
Qandeel Khan <com.qandeelhaider@gmail.com>
2020-12-25 05:22:47 +0500

    add featers to new blog post from frontend

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
